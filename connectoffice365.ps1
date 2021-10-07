22b9b28b9636bddf23734b64d21978952d4c2fe6
Berei László <70773393+bereilaszlo@users.noreply.github.com>
2021-10-07 15:39:36 +0200

    Create connectoffice365.ps1

---
+++ connectoffice365.ps1
@@ -0,0 +1,5 @@
+$Cred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Cred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+# Run any commands you want here
+Remove-PSSession $Session
