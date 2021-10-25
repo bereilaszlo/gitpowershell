0dd5c791d22145fed65a2af5fcd3f613df1f8950
Berei László <70773393+bereilaszlo@users.noreply.github.com>
2021-10-25 16:40:22 +0200

    Add files via upload

---
+++ Remove External User.ps1
@@ -0,0 +1,14 @@
+﻿#Remove External User
+
+$credential = get-credential
+
+Connect-msolservice -credential $credential
+
+Connect-sposervice -url https://bennettbizdemo-admin.sharepoint.com -credential $credential
+
+get-spoexternaluser -position 0 -pagesize 30 | select Displayname,email | format-table
+
+$user = Get-SPOExternalUser -Filter sbfortesting@outlook.com
+
+Remove-SPOExternalUser -UniqueIDs @($user.UniqueId)
+
---
+++ SPO Site Creation.ps1
@@ -0,0 +1,18 @@
+﻿#Login
+
+$credential = Get-Credential
+
+Connect-MsolService -Credential $credential
+
+#Commands to connect to SharePoint Online.
+
+Connect-SPOService -Url https://bennettbizdemo-admin.sharepoint.com -credential $credential
+
+
+#Example for SPO Collection Creation & Deletion
+
+New-SPOSite -Url https://bennettbizdemo.sharepoint.com/sites/demosite3 -Owner o365admin@bennettbizdemo.onmicrosoft.com -StorageQuota 1024
+
+Remove-SPOSite -Identity https://bennettbizdemo.sharepoint.com/sites/demosite3 -NoWait
+
+Restore-SPODeletedSite -Identity https://bennettbizdemo.sharepoint.com/sites/demosite3
